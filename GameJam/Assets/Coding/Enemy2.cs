2509c216f32208805cfb6905ede5fd112f19f946
Quang Pham <quanghuy25899@gmail.com>
2019-01-26 23:22:15 -0800

    Second commit

---
+++ GameJam/Assets/Coding/Enemy2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Enemy1").transform;
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ GameJam/Assets/Coding/EnemyFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+
+    private Transform target;
+
+    void Start() {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update() {
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance) {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
